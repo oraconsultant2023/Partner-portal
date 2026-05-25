fa7d8775259e5d6836fdedf03bd420b68a7663b1
Syed Ahmed <syed@techcure.io>
2026-05-25 21:16:21 +0500

    Added applications g

--- app/routes/app.applications.tsx
+++ app/routes/app.applications.tsx
@@ -1,13 +1,12 @@
 import { json } from "@remix-run/node";
-import { authenticate } from "../shopify.server";
+// import { authenticate } from "../shopify.server";
 
 export async function loader({ request }: any) {
-
-  const { admin } = await authenticate.admin(request);
+  // Temporarily bypass auth to test routing
+  // const { admin } = await authenticate.admin(request);
 
   return json({
     success: true,
     message: "Applications route working"
   });
-
 }
