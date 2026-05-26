5c9f2c54ee7de1d9e555f3ea6fb73911668e6f51
Syed Ahmed <syed@techcure.io>
2026-05-26 15:58:00 +0500

    Testing campaigns route

---
+++ app/routes/app.campaigns.tsx
@@ -0,0 +1,19 @@
+export default function Campaigns() {
+
+  return (
+
+    <div
+      style={{
+        padding: "40px"
+      }}
+    >
+
+      <h1>
+        Campaign Route Working
+      </h1>
+
+    </div>
+
+  );
+
+}
